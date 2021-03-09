818cf0b07bd5ff78a54c9cac6acf1d6f090e391a
Francisco <pancho.luarca@gmail.com>
2021-03-08 21:47:30 -0600

    primer cambio

--- src/bdcelaju/BdCelaju.java
+++ src/bdcelaju/BdCelaju.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package bdcelaju;
 
 /**
@@ -15,7 +11,8 @@ public class BdCelaju {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hola git");
+        //probando el git
     }
     
 }
