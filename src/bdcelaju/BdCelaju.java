3be9949658e157393c9050a99c983a22f07cbffa
Francisco <pancho.luarca@gmail.com>
2021-03-08 21:50:35 -0600

    empezando

--- src/bdcelaju/BdCelaju.java
+++ src/bdcelaju/BdCelaju.java
@@ -12,12 +12,7 @@ public class BdCelaju {
      */
     public static void main(String[] args) {
         System.out.println("Hola git");
-        //probando el git 
-        System.out.println("Hola git");
-        System.out.println("Hola git");
-        System.out.println("Hola git");
-        System.out.println("Hola git");
-        System.out.println("Hola git");
+
         
     }
     
